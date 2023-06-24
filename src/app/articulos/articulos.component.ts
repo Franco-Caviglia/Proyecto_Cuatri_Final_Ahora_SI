b46acbfeb01f1082d2fc53ffc08765f4f7b1fc9b
Franco-Caviglia <francocaviglia77@gmail.com>
2023-06-24 14:20:01 -0300

    Cards armadas

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ServicioCarritoService } from '../servicio-carrito.service';
 
 interface Articulo{
   imagen?: String; //el '?' indica que esta propiedad puede o no tener un valor;
   marca: String; //contendra la marca del articulo;
   peso: String; //contendra el peso del articulo;
-  medida: String; //contendra la medida del articulo;
+  medida?: String; //contendra la medida del articulo;
+  rodado?: String; //contendra el rodado de la rueda;
   precio: Number; //contendra el precio del articulo;
   tipo: String; //contendra que tipo de articulo es, para identificarlo;
   material: String; //contendra el material con el que fue fabricado;
@@ -16,8 +18,8 @@ interface Articulo{
   templateUrl: './articulos.component.html',
   styleUrls: ['./articulos.component.css']
 })
-export class ArticulosComponent {
-  title = 'DXBikes';
+export class ArticulosComponent implements OnInit{
+  title = 'Articulos';
 
   @Input() dataEntrante: any;
 
@@ -35,7 +37,7 @@ export class ArticulosComponent {
         medida: '29', 
         precio: 53994,
         tipo: 'Cuadro',
-        material: ''
+        material: ''        
       },
       {
         imagen: '../assets/Cuadros/Scott.jpg',
@@ -58,7 +60,7 @@ export class ArticulosComponent {
       {
         imagen:'../assets/Ruedas/R26.jpg',
         marca:"Foxter 4.20",
-        medida:"Rodado 26",
+        rodado:"Rod 26",
         material:"Aluminio",
         precio:30.987,
         tipo: 'Rueda',
@@ -67,7 +69,7 @@ export class ArticulosComponent {
       {
         imagen:'../assets/Ruedas/R28.jpg',
         marca:"Vrc 701 Houston",
-        medida:"Rodado 28",
+        rodado:"Rod 28",
         material:"Aluminio",
         precio:16.500,
         tipo: 'Rueda',
@@ -76,7 +78,7 @@ export class ArticulosComponent {
       {
         imagen:'../assets/Ruedas/R29.jpg',
         marca:" Shimano Deore Xt",
-        medida:"Rodado 29",
+        rodado:"Rod 29",
         material:"Aluminio",
         precio:158.336,
         tipo: 'Rueda',
