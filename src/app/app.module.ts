a8e277768a71991ba790141d3b5f4b4c55fdbd04
Franco-Caviglia <francocaviglia77@gmail.com>
2023-06-24 15:04:30 -0300

    Acomodado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticulosComponent } from './articulos/articulos.component';
+import { Router, RouterModule } from '@angular/router';
+import { CarritoComponent } from './carrito/carrito.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticulosComponent
+    ArticulosComponent,
+    CarritoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
